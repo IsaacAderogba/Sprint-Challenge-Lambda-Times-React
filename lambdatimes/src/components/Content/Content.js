1ddca495db8b1f0cfe4093ee3b02f71388cf84bd
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-07 11:02:07 +0100

    Render carousel at top of content component container

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -42,12 +42,12 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
+        <Carousel />
         <Tabs
           tabs={this.state.tabs}
           selectedTab={this.state.selected}
           selectTabHandler={this.changeSelected}
         />
-        <Carousel />
         <Cards cards={this.filterCards()} />
       </div>
     );
