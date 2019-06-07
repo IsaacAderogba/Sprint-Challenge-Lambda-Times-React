08086ae2f3f75bba74d55d096816515e4cb01cee
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-07 08:46:32 +0100

    Render the tabs onto the screen

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Tabs from './Tabs';
-import Cards from './Cards';
+import Tabs from "./Tabs";
+import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
-import { tabData, cardData } from '../../data';
+import { tabData, cardData } from "../../data";
 
 export default class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: 'all',
-      tabs: [],
-      cards: []
+      selected: "all",
+      tabs: tabData,
+      cards: cardData
     };
   }
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+
+    // below code doesn't work for some reason.
+    // this.setState({ tabs: tabData, cards: cardData });
   }
 
   changeSelected = tab => {
@@ -48,7 +51,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
         <Cards cards={this.filterCards()} />
       </div>
     );
